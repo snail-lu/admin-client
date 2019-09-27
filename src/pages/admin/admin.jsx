1ee5cc6ac38bab2bbdfcb06839cdff7de0e29d76
路明 <snaillu@yeah.net>
2019-09-27 10:33:59 +0800

    admin组件中添加登陆判断,未登陆时，重定向到登陆页

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import {Redirect} from 'react-router-dom';
+import memoryUtils from '../../utils/memoryUtils';
 
 /**
  * 后台 管理的路由组件
  */
 class Admin extends Component {
     render() {
+        const user = memoryUtils.user;
+        if(!user || !user._id){
+            return <Redirect to='/' />
+        }
         return (
             <div>
-                Admin
+                Hello {user.username}
             </div>
         );
     }
