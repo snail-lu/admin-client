e0424199e4744eb992938333a2da176acd32d208
路明 <snaillu@yeah.net>
2019-09-29 17:00:45 +0800

    新增获取天气请求方法

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,25 @@
  */
 
 import ajax from "./ajax";
+import jsonp from 'jsonp'; 
+import { message } from "antd";
 
 //登录
 export const reqLogin = (username,password) => ajax('/login',{username,password},'POST');
 
-//
+//获取天气
+export const reqWeather = (city) => {
+    return new Promise((resolve,reject)=>{
+        const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+        jsonp(url,{},(err,data)=>{
+            if(!err && data.status==='success'){
+                const {dayPictureUrl,weather} = data.results[0].weather_data[0];
+                resolve({dayPictureUrl,weather});
+            }else{
+                message.error('获取天气信息失败')
+            }
+        })
+    })   
+}
+
+reqWeather('北京')
