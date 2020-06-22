6488d1938974f8ab4fd96eab73ee1276fb36d0d6
路明 <snaillu@yeah.net>
2020-06-22 19:14:38 +0800

    新增部分接口请求方法

--- src/api/index.js
+++ src/api/index.js
@@ -6,29 +6,31 @@ import ajax from "./ajax";
 import jsonp from 'jsonp'; 
 import { message } from "antd";
 
+const apiBase = '/api';
+
 //登录
-export const reqLogin = (username,password) => ajax('/admin/login',{username,password},'POST');
+export const reqLogin = (username,password) => ajax(apiBase+'/admin/login',{username,password},'POST');
 
 //注册
-export const reqRegister = (username,password,adminLevel,email,avatar) => ajax('/admin/register',{username,password,adminLevel,email,avatar},'POST');
+export const reqRegister = (username,password,adminLevel,email,avatar) => ajax(apiBase+'/admin/register',{username,password,adminLevel,email,avatar},'POST');
 
 //添加分类
-export const reqAddCategory = (categoryName,parentId) => ajax('/category/add',{categoryName,parentId},'POST');
+export const reqAddCategory = (categoryName,parentId) => ajax(apiBase+'/category/add',{categoryName,parentId},'POST');
 
 //获取分类
-export const reqCategoryList = (parentId) => ajax('/category/list',{parentId},'POST');
+export const reqCategoryList = (parentId) => ajax(apiBase+'/category/list',{parentId},'POST');
 
 //更新分类
-export const reqUpdateCategory = (categoryId,categoryName) => ajax('/category/update',{categoryId,categoryName},'POST');
+export const reqUpdateCategory = (categoryId,categoryName) => ajax(apiBase+'/category/update',{categoryId,categoryName},'POST');
 
 //获取管理员列表
-export const reqAdminList = () => ajax('/admin/list',{},'POST');
+export const reqAdminList = () => ajax(apiBase+'/admin/list',{},'POST');
 
 //删除管理员
-export const reqDeleteAdmin = (id) => ajax('/admin/delete',{id},'POST');
+export const reqDeleteAdmin = (id) => ajax(apiBase+'/admin/delete',{id},'POST');
 
 //修改管理员信息
-export const reqUpdateAdmin = ({_id,username,email,adminLevel,avatar}) => ajax('/admin/update',{_id,username,email,adminLevel,avatar},'POST');
+export const reqUpdateAdmin = ({_id,username,email,adminLevel,avatar}) => ajax(apiBase+'/admin/update',{_id,username,email,adminLevel,avatar},'POST');
 
 //获取天气
 export const reqWeather = (city) => {
@@ -44,5 +46,29 @@ export const reqWeather = (city) => {
         })
     })   
 }
+
+//新增配置
+export const addConfig = (configName,configKey,configValue,configSorts) => ajax(apiBase+'/config/add',{configName,configKey,configValue,configSorts},'POST');
+
+//修改配置
+export const saveConfig = (id,configName,configKey,configValue,configSorts,reviser) => ajax(apiBase+'/config/edit',{id,configName,configKey,configValue,configSorts,reviser},'POST');
+
+//配置列表
+export const reqConfigList = () => ajax(apiBase+'/config/list',{},'POST');
+
+//获取配置信息
+export const getConfigInfo = (id) => ajax(apiBase+'/config/detail',{ id }, 'POST');
+
+//文章列表
+export const reqArticlesList = () => ajax(apiBase+'/articles/list',{},'POST');
+
+//文章详情
+export const reqArticleInfo = (id) => ajax(apiBase+'/articles/detail',{ id },'POST');
+
+//新增文章
+export const addArticle = (title,author,content) => ajax(apiBase+'/articles/add', {title,author,content},'POST');
+
+export const saveArticle = (id, title, author, content) => ajax(apiBase+'/articles/save', { id, title, author, content }, 'POST');
+
 
 reqWeather('北京')
