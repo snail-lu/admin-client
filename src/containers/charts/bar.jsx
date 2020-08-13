6c6db6c8ff5f8c391f56b2cf5c79c9b2f7a51e36
路明 <snaillu@yeah.net>
2020-08-13 20:32:42 +0800

    引入bizcharts

--- src/containers/charts/bar.jsx
+++ src/containers/charts/bar.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { Chart, Interval, Tooltip } from 'bizcharts';
+
 
 class Bar extends Component {
+    data = [
+        { year: '1951 年', sales: 38 },
+        { year: '1952 年', sales: 52 },
+        { year: '1956 年', sales: 61 },
+        { year: '1957 年', sales: 45 },
+        { year: '1958 年', sales: 48 },
+        { year: '1959 年', sales: 38 },
+        { year: '1960 年', sales: 38 },
+        { year: '1962 年', sales: 38 },
+    ];
     render() {
         return (
             <div>
-                柱状图
+                <Chart height={400} autoFit data={this.data} interactions={['active-region']} padding={[30, 30, 30, 50]} >
+                    <Interval position="year*sales" />
+                    <Tooltip shared />
+                </Chart>
             </div>
         );
     }
--- src/containers/user/user.jsx
+++ src/containers/user/user.jsx
@@ -1,10 +1,102 @@
 import React, { Component } from 'react';
+import { Button, Table, Card, Input, Icon } from 'antd';
+
+const { Search } = Input;
 
 class User extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            
+        }
+    }
+
+    columns = [
+        {
+            title: '用户名',
+            dataIndex: 'name',
+            key: 'name',
+            render: text => <a>{text}</a>,
+        },
+        {
+            title: '角色',
+            dataIndex: 'role',
+            key: 'role',
+        },
+        {
+            title: '性别',
+            dataIndex: 'gender',
+            key: 'gender',
+        },
+        {
+            title: '手机号',
+            dataIndex: 'mobile',
+            key: 'addmobileress',
+        },
+        {
+            title: '所在地',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: '状态',
+            dataIndex: 'status',
+            key: 'status',
+        },
+        {
+            title: 'Action',
+            dataIndex: '',
+            key: 'x',
+            render: () => <a>禁用</a>,
+          },
+      ];
+      tableData = [
+        {
+          key: '1',
+          name: 'John Brown',
+          age: 32,
+          address: 'New York No. 1 Lake Park',
+          tags: ['nice', 'developer'],
+        },
+        {
+          key: '2',
+          name: 'Jim Green',
+          age: 42,
+          address: 'London No. 1 Lake Park',
+          tags: ['loser'],
+        },
+        {
+          key: '3',
+          name: 'Joe Black',
+          age: 32,
+          address: 'Sidney No. 1 Lake Park',
+          tags: ['cool', 'teacher'],
+        },
+      ]
     render() {
+        const title = (
+            <Search placeholder="关键词" onSearch={value => console.log(value)} enterButton style={{ width: 400}}/>
+          )
+          const extra = (
+                <div>
+                    <Button type="primary"><Icon type='plus'></Icon>添加</Button> 
+                    <Button type="primary"><Icon type='delete'></Icon>删除</Button> 
+                    <Button type="primary"><Icon type='upload'></Icon>导入</Button> 
+                    <Button type="primary"><Icon type='download'></Icon>导出</Button>
+                </div>
+            );
         return (
-            <div>
-                用户管理
+            <div className="activity-set-container">
+                <Card title={title} extra={extra}>
+              <Table 
+                bordered
+                rowKey="_id"
+                dataSource={this.tableData} 
+                loading={this.state.loading}
+                columns={this.columns}
+                style={{fontSize:15}}
+              />
+            </Card>
             </div>
         );
     }
